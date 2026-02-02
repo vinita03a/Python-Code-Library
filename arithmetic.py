769d54b2caa361342228f079c3df0474808d07e9
Vinita Awahd <160589847+vinita03a@users.noreply.github.com>
2026-02-02 12:59:10 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+# 02-02-2026
+
+'''
+Docstring for Datas.py.Opertaors.arithmetic
+
+arithmetic - (+ , - , * , / , // , ** , %)
+'''
+
+n1 = 16
+n2 = 5
+
+add = n1+n2
+
+print("Addition - ",add)
+
+print("Subraction - ",n1-n2)
+
+print("Product -",n1*n2)
+
+# division - quotient in form of float value
+print("Division - ",n1/n2)
+
+# floor division - integer part of quotient 
+print("Floor division -",n1//n2)
+
+#modulus (%) - remainder
+print("Modulus - ",n1%n2)
+
+# exponential (**)
+
+a=6
+print("Exponential - ",a**3)
